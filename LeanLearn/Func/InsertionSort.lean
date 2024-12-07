9bb1d9014ddc0e87429724973c310aa62547b3ae
bernborgess <bernborgess@outlook.com>
2024-12-07 11:11:20 -0300

    Scaffold for quick_sort_correct

---
+++ LeanLearn/Func/InsertionSort.lean
@@ -0,0 +1,82 @@
+/-
+Copyright (c) 2024 Tomaz Mascarenhas. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Tomaz Mascarenhas
+-/
+import Mathlib.Data.List.Sort
+import Mathlib.Tactic.Linarith
+
+/-!
+# Timed Insertion Sort
+  This file defines a new version of Insertion Sort that, besides sorting the input list, counts the
+  number of comparisons made through the execution of the algorithm. Also, it presents proofs of
+  its time complexity and its equivalence to the one defined in Data/List/Sort.lean
+ ## Main Definition
+  - Timed.insertion_sort : list α → (list α × ℕ)
+## Main Results
+  - Timed.insertion_sort_complexity :
+      ∀ l : list α, (Timed.insertionSort r l).snd ≤ l.length * l.length
+  - Timed.insertion_sort_equivalence :
+      ∀ l : list α, (Timed.insertionSort r l).fst = List.insertionSort r l
+-/
+
+namespace Timed
+
+universe u
+
+variable {α : Type u} (r : α → α → Prop) [DecidableRel r]
+local infixl:50 " ≼ " => r
+
+@[simp] def orderedInsert (a : α) : List α → (List α × Nat)
+  | []      => ([a], 0)
+  | b :: l => if a ≼ b then (a :: b :: l, 1)
+              else let (l', n) := orderedInsert a l
+                   (b :: l', n + 1)
+
+@[simp] def insertionSort : List α → (List α × Nat)
+  | [] => ([], 0)
+  | (h :: t) => let (l', n)  := insertionSort t
+                let (l'', m) := orderedInsert r h l'
+                (l'', n + m)
+
+theorem orderedInsert_complexity (a : α) :
+    ∀ l : List α, (orderedInsert r a l).snd ≤ l.length
+  | []     => by simp
+  | b :: l' => by
+    simp only [orderedInsert, List.length_cons]
+    split_ifs with h
+    · simp
+    · simp [orderedInsert_complexity a l']
+
+#check List.orderedInsert
+#check List.Sorted
+#check List.Disjoint
+
+theorem orderedInsert_equivalence (a : α) : ∀ l : List α,
+    (orderedInsert r a l).fst = List.orderedInsert r a l
+  | [] => by simp
+  | b :: l' => by
+    simp only [orderedInsert, List.orderedInsert]
+    split_ifs with h
+    · rfl
+    · simp [orderedInsert_equivalence a l']
+
+theorem orderedInsert_increases_length (a : α) : ∀ l : List α,
+    (orderedInsert r a l).fst.length = l.length + 1
+  | [] => by simp
+  | b :: l' => by
+    simp only [orderedInsert, List.length_cons]
+    split_ifs with h
+    · rfl
+    · simp [orderedInsert_increases_length a l']
+
+theorem insertionSort_preserves_length : ∀ l : List α,
+    (insertionSort r l).fst.length = l.length := fun l =>
+  match l with
+  | [] => by simp
+  | a :: l' => by
+    simp only [insertionSort, List.length_cons]
+    rw [orderedInsert_increases_length r a (insertionSort r l').fst]
+    simp [insertionSort_preserves_length l']
+
+
--- LeanLearn/Func/QuickSort.lean
+++ LeanLearn/Func/QuickSort.lean
@@ -1,12 +1,13 @@
 import Mathlib.Tactic.SimpRw
+import Mathlib.Data.List.Sort
 
 theorem List.length_filter_tail_lt.{u} {α : Type u} (p : α → Bool) (xs : List α)
   (xs_not_empty : xs ≠ [])
   : (xs.tail.filter p).length < xs.length := by
   obtain ⟨h,t,xs_eq_ht⟩ := xs.exists_cons_of_ne_nil xs_not_empty
-  apply Nat.lt_of_le_of_lt
+  apply @Nat.lt_of_le_of_lt ((filter p xs.tail).length) (t.length) (xs.length)
   . rw [xs_eq_ht]
-    exact length_filter_le p t
+    exact length_filter_le p (h :: t).tail
   . simp_rw [xs_eq_ht]
     exact Nat.lt_add_one t.length
 
@@ -29,20 +30,7 @@ def quick_sort [LT α] [LE α]
 
 termination_by xs => xs.length
 
-/- theorem complementary [LT α] [LE α] -/
-/-   [DecidableRel ((·<·) : α → α → Prop)] -/
-/-   [DecidableRel ((·≥·) : α → α → Prop)] -/
-/-   (x : α) -/
-/-   : ∀y:α, (y < x) ∨ (y ≥ x) -/
-
-theorem Nat.complementary
-  (x : Nat)
-  : ∀y:Nat, (y < x) ∨ (y ≥ x)
-  := by
-  intro y
-
-  exact Nat.lt_or_ge y x
-#check Nat.lt_or_ge
+#eval quick_sort [1,2,5,4,3]
 
 def partition_lt_ge_prop
   (x : Nat)
@@ -52,7 +40,9 @@ def partition_lt_ge_prop
   let more := l.filter (· ≥ x)
   ∀ y : Nat, y ∈ l → y ∈ less ∨ y ∈ more
 
--- #check le_of_not_lt
+#check IsTotal
+#check LE.isTotal
+#check instIsTotalGe
 
 example (x : Nat) (l : List Nat) : partition_lt_ge_prop x l := by
   simp only [partition_lt_ge_prop, List.mem_filter, decide_eq_true_eq, ge_iff_le]
@@ -70,8 +60,7 @@ theorem List.filter_lt_or_ge.{u} {α : Type u} (p : α → Bool) (xs : List α)
   /- apply Nat.lt_of_le_of_lt -/
   sorry
 
-
-theorem quick_sort_length_eq
+theorem quick_sort_preserves_length
   [LT α] [LE α]
   [DecidableRel ((·<·) : α → α → Prop)]
   [DecidableRel ((·≥·) : α → α → Prop)]
@@ -82,8 +71,35 @@ theorem quick_sort_length_eq
   | nil => simp only [List.length_nil, List.length_eq_zero, quick_sort]
   | cons h t tail_hi =>
     simp only [List.length_cons, quick_sort]
-
-
     sorry
 
-#eval quick_sort [1,2,5,4,3]
+
+#check List.Disjoint
+
+example : [1,2,3].Sorted (·<·) 
+  := by
+  apply List.sorted_cons_cons.mpr
+  simp only [Nat.lt_add_one, List.sorted_cons, List.mem_singleton, forall_eq, List.sorted_singleton, and_self]
+  trivial
+
+#check List.Sorted
+#check List.sorted_nil
+#check List.sorted_singleton
+#check List.sorted_cons
+#check List.sorted_cons_cons
+
+theorem quick_sort_correct.{u} {α : Type u}
+  [LT α] [LE α]
+  [DecidableRel ((·<·) : α → α → Prop)]
+  [DecidableRel ((·≥·) : α → α → Prop)]
+  : ∀ l : List α, List.Sorted (·<·) (quick_sort l)
+  := by
+  intro l
+  induction l with
+  | nil => rw [quick_sort]; exact List.sorted_nil
+  | cons x y h => 
+    rw [quick_sort]
+    /- ? -/
+    /- apply List.sorted_cons.mp -/
+    sorry
+
